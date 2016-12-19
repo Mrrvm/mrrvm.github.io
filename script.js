d7b93838301ab24dfb65e9370322164825271db1
Mrrvm <mrrvm@hotmail.com>
2016-12-19 23:40:22 +0000

    Got a home page

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("about").onclick = function () {
+    location.href = "about.html";
+};
+document.getElementById("portefolio").onclick = function () {
+    location.href = "portefolio.html";
+};
+document.getElementById("contact").onclick = function () {
+    location.href = "contact.html";
+};
